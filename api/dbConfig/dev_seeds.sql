42469e3070d3a7cf57fd75d4a11066ca0517cd4c
Sam Hardiment <samuelghardiment@gmail.com>
2022-06-24 15:55:59 +0100

    add a temp seed file for dbconfig

---
+++ api/dbConfig/dev_seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO members (name) 
+VALUES 
+('Sam'),
+('Shoreen'),
+('William'),
+('Amir'),
+('Ali');
---
+++ db/migrations/1_setup.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS members;
+
+CREATE TABLE members (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+);
